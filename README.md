7e54bef0abbab673bf167fbe60b522bbae223506
Leo Nistor <leo.nistor@gmail.com>
2020-12-19 22:42:26 +0200

    chapter 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chat app from "Go Programming Blueprints by Mat Ryer"
+
+2nd edition.
+
+https://github.com/matryer/goblueprints/tree/master
---
+++ room.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/leonistor/chat/trace"
+)
+
+type room struct {
+	// incoming messages that should be forwarded to other clients
+	forward chan []byte
+	// clients wishing to join the room
+	join chan *client
+	// clients wishing to leave the room
+	leave chan *client
+	// current clients
+	clients map[*client]bool
+	// trace room activity
+	tracer trace.Tracer
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: map[*client]bool{},
+		tracer:  trace.Off(),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			r.tracer.Trace("New client joined")
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			r.tracer.Trace("Client left")
+		case msg := <-r.forward:
+			r.tracer.Trace("Message received: ", string(msg))
+			for client := range r.clients {
+				client.send <- msg
+				r.tracer.Trace(" -- sent to client")
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServerHTTP: ", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() {
+		r.leave <- client
+	}()
+	go client.write()
+	client.read()
+}
