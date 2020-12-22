c24dd1b50076b8a7a7561dd5742db5f7bb46863e
Leo Nistor <leo.nistor@gmail.com>
2020-12-22 16:29:57 +0200

    GravatarAvatar

--- avatar.go
+++ avatar.go
@@ -1,6 +1,12 @@
 package main
 
-import "errors"
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
 
 // ErrNoAvatarURL is the error retrurned when
 // an Avatar instance cannot provide an avatar URL
@@ -28,6 +34,26 @@ func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
 	}
 	if urlStr, ok := url.(string); ok {
 		return urlStr, nil
+	}
+	return "", ErrNoAvatarURL
+}
+
+// GravatarAvatar gets avatar from gravatar.com
+type GravatarAvatar struct{}
+
+// UseGravatarAvatar selects gravatar as avatar source
+var UseGravatarAvatar GravatarAvatar
+
+// GetAvatarURL for GravatarAvatar
+func (GravatarAvatar) GetAvatarURL(c *client) (string, error) {
+	email, ok := c.userData["email"]
+	if !ok {
+		return "", ErrNoAvatarURL
+	}
+	if emailStr, ok := email.(string); ok {
+		m := md5.New()
+		io.WriteString(m, strings.ToLower(emailStr))
+		return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
 	}
 	return "", ErrNoAvatarURL
 }
